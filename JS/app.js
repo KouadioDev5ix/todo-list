6c39d26abe08979362d9fe722f69cb40fb5bf579
Kouadio Kouadio Alfred <alfredkouadio1997@gmail.com>
2024-01-31 15:25:22 +0000

    New file add

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+const el = document.querySelector("h1");
+const isValid = false;
+const number = 125e9;
+let x = BigInt("123456789012345678901234567890");
+console.log(typeof el);
+console.log(typeof x);
+console.log(number);
